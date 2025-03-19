e0d9845b3e908c35f83ff43bc1100c48268938f0
Fabian Reifler <72970313+reifab@users.noreply.github.com>
2025-03-19 13:09:53 +0100

    zwischenstand 1309

--- main.ts
+++ main.ts
@@ -6,47 +6,36 @@ function warte_5_Sekunden_mit_Anzeige () {
     }
     smartfeldAktoren.oledClear()
 }
+let distanzSensorZuSeife = 0
 let seifenstandInProzent = 100
-led.plotBarGraph(
-seifenstandInProzent,
-100
-)
 smartfeldAktoren.oledInit(128, 64)
 smartfeldAktoren.oledClear()
-smartfeldAktoren.oledWriteStr("Verbinde")
-IoTCube.LoRa_Join(
-eBool.enable,
-eBool.enable,
-10,
-8
-)
-while (!(IoTCube.getStatus(eSTATUS_MASK.JOINED))) {
-    smartfeldAktoren.oledWriteStr(".")
-    basic.pause(1000)
-}
-smartfeldAktoren.oledClear()
-smartfeldAktoren.oledWriteStr("Verbunden!")
-basic.pause(3000)
-smartfeldAktoren.oledClear()
-IoTCube.addUnsignedInteger(eIDs.ID_0, seifenstandInProzent)
-IoTCube.SendBufferSimple()
-warte_5_Sekunden_mit_Anzeige()
-basic.forever(function () {
-    if (input.buttonIsPressed(Button.A)) {
-        seifenstandInProzent = seifenstandInProzent - 20
-        if (seifenstandInProzent < 0) {
-            seifenstandInProzent = 0
-        }
-        IoTCube.addUnsignedInteger(eIDs.ID_0, seifenstandInProzent)
-        IoTCube.SendBufferSimple()
-        warte_5_Sekunden_mit_Anzeige()
-        led.plotBarGraph(
-        seifenstandInProzent,
-        100
-        )
+if (false) {
+    smartfeldAktoren.oledWriteStr("Verbinde")
+    IoTCube.LoRa_Join(
+    eBool.enable,
+    eBool.enable,
+    10,
+    8
+    )
+    while (!(IoTCube.getStatus(eSTATUS_MASK.JOINED))) {
+        smartfeldAktoren.oledWriteStr(".")
+        basic.pause(1000)
     }
-    if (input.buttonIsPressed(Button.B)) {
-        seifenstandInProzent = 100
+    smartfeldAktoren.oledClear()
+    smartfeldAktoren.oledWriteStr("Verbunden!")
+    basic.pause(3000)
+    smartfeldAktoren.oledClear()
+    IoTCube.addUnsignedInteger(eIDs.ID_0, seifenstandInProzent)
+    IoTCube.SendBufferSimple()
+    warte_5_Sekunden_mit_Anzeige()
+}
+basic.forever(function () {
+    smartfeldAktoren.oledClear()
+    distanzSensorZuSeife = smartfeldSensoren.measureInCentimetersV2(DigitalPin.P0)
+    smartfeldAktoren.oledWriteNum(Math.round(distanzSensorZuSeife))
+    smartfeldAktoren.oledWriteStr(" cm")
+    if (false) {
         IoTCube.addUnsignedInteger(eIDs.ID_0, seifenstandInProzent)
         IoTCube.SendBufferSimple()
         warte_5_Sekunden_mit_Anzeige()
